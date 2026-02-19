db883f29b6dcf26b70633435eeb0ff8a79d90d14
Vitaly Tulin <me@tulvit.net>
2026-02-19 08:53:37 +0300

    Text change

--- src/locales/en/common.ts
+++ src/locales/en/common.ts
@@ -1,4 +1,4 @@
 export const commonEn = {
     siteTitle: "Art History Timeline",
-    siteSlogan: "History timeline — ancient times to modern era"
+    siteSlogan: "Historical Timeline: From Ancient Times to the Modern Era"
   };
